b3891232df31b6ad3f3520176534400b58c8e6af
Shinichi <miyakawashi@gmail.com>
2022-03-13 19:29:57 -0400

    manifest.json and service-worker start

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,20 @@
+const APP_PREFIX = '';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const FILES_TO_CACHE = [
+    './index.html',
+    './js/index.js',
+    './css/style.css'
+];
+
+self.addEventListener('fetch', function(e) {
+
+});
+
+self.addEventListener('install', function(e) {
+
+});
+
+self.addEventListener('activate', function(e) {
+
+});
